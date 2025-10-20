7b74f4084639ad0b8643c5ff8708451c3e94a9da
Swati Bhatia <swatibhatia626@gmail.com>
2025-10-20 22:18:47 +0530

    error case in middleware , next is going to next handler but res is already sent

--- src/app.js
+++ src/app.js
@@ -2,33 +2,13 @@ const express = require("express")
 const app = express()
 
 // only for this particular route /user
-app.get('/user/:userId/:password', (req, res) => {
+app.get('/user', (req, res, next) => {
   console.log(req.params, req.query)
-  res.send({
-    firstName: "Swati",
-    lastName: "Bhatia",
-  })
+  res.send("First handler for /user")
+  next()
+}, (req, res, next)=>{
+    res.send("Second handler for /user")
 })
-
-// app.post("/user", (req, res) => {
-//   // ADDED TO DATABSE
-//   res.send("Successfully Added User")
-// })
-
-// app.delete("/user", (req, res) => {
-//   // REMOVED FROM DATABASE
-//   res.send("Successfully Deleted User")
-// })
-
-// app.patch("/user", (req, res) => {
-//   // UPDATED IN DATABASE
-//   res.send("Successfully Updated User")
-// })
-
-// can be used for all the methods(GET, POST, DELETE, PATCH, etc.) for a particular route and any route that starts with /user/anything
-// app.use("/user", (req, res) => {
-//   res.send("Hello From Server /user")
-// })
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777")
