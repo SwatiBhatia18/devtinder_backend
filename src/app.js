c798d44c838ea2db197feab259a1badaf982c2a4
Swati Bhatia <swatibhatia626@gmail.com>
2025-10-21 02:02:52 +0530

    sequence of error handling matters , always use at last to catch any error if missed in try catch

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
 const express = require("express")
 const app = express()
 
-app.get("/getAllUser", (req, res, next) => {
-  try {
-    throw new Error("Some error occurred while fetching users")
-    res.send("List of all users")
-  } catch (err) {
-    res.status(500).send("Something broke!")
-  }
-})
-
 app.use((err, req, res, next) => {
   console.error(err.stack)
   if (err) res.status(500).send("Something broke using err !")
+})
+
+app.get("/getAllUser", (req, res, next) => {
+    throw new Error("Some error occurred while fetching users")
 })
 
 app.listen(7777, () => {
