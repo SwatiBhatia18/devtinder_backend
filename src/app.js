70a85c515d3bd3964d427175a3a6ee1749873a54
Swati Bhatia <swatibhatia626@gmail.com>
2025-10-20 22:14:21 +0530

    request params and query , and regex inside routes

--- src/app.js
+++ src/app.js
@@ -2,32 +2,33 @@ const express = require("express")
 const app = express()
 
 // only for this particular route /user
-app.get("/user", (req, res) => {
+app.get('/user/:userId/:password', (req, res) => {
+  console.log(req.params, req.query)
   res.send({
     firstName: "Swati",
     lastName: "Bhatia",
   })
 })
 
-app.post("/user", (req, res) => {
-  // ADDED TO DATABSE
-  res.send("Successfully Added User")
-})
+// app.post("/user", (req, res) => {
+//   // ADDED TO DATABSE
+//   res.send("Successfully Added User")
+// })
 
-app.delete("/user", (req, res) => {
-  // REMOVED FROM DATABASE
-  res.send("Successfully Deleted User")
-})
+// app.delete("/user", (req, res) => {
+//   // REMOVED FROM DATABASE
+//   res.send("Successfully Deleted User")
+// })
 
-app.patch("/user", (req, res) => {
-  // UPDATED IN DATABASE
-  res.send("Successfully Updated User")
-})
+// app.patch("/user", (req, res) => {
+//   // UPDATED IN DATABASE
+//   res.send("Successfully Updated User")
+// })
 
 // can be used for all the methods(GET, POST, DELETE, PATCH, etc.) for a particular route and any route that starts with /user/anything
-app.use("/user", (req, res) => {
-  res.send("Hello From Server /user")
-})
+// app.use("/user", (req, res) => {
+//   res.send("Hello From Server /user")
+// })
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777")
