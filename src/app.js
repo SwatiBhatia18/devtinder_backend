3b90f9ee3ac0a90791795de497520aa16f99dedd
Swati Bhatia <swatibhatia626@gmail.com>
2025-10-20 22:23:02 +0530

    use case where next is implemented and again the below code of next is then run

--- src/app.js
+++ src/app.js
@@ -2,13 +2,19 @@ const express = require("express")
 const app = express()
 
 // only for this particular route /user
-app.get('/user', (req, res, next) => {
-  console.log(req.params, req.query)
-  res.send("First handler for /user")
-  next()
-}, (req, res, next)=>{
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("First handler for /user")
+    next()
+    console.log("First handler for after next")
+    res.send("First handler for /user")
+  },
+  (req, res, next) => {
+    console.log("Second handler for /user")
     res.send("Second handler for /user")
-})
+  }
+)
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777")
