d811a19662164dae97bbeb608d2976b206761122
Swati Bhatia <swatibhatia626@gmail.com>
2025-10-20 17:11:25 +0530

    exploring get , post and delete

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
 const express = require("express")
 const app = express()
 
-app.use('/test', (req,res)=>{
-    res.send("Hello From Server /test")
+// only for this particular route /user
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Swati",
+    lastName: "Bhatia",
+  })
 })
 
-app.listen(7777, ()=>{
-    console.log("Server is running on port 7777")
+app.post("/user", (req, res) => {
+  // ADDED TO DATABSE
+  res.send("Successfully Added User")
+})
+
+app.delete("/user", (req, res) => {
+  // REMOVED FROM DATABASE
+  res.send("Successfully Deleted User")
+})
+
+app.patch("/user", (req, res) => {
+  // UPDATED IN DATABASE
+  res.send("Successfully Updated User")
+})
+
+// can be used for all the methods(GET, POST, DELETE, PATCH, etc.) for a particular route and any route that starts with /user/anything
+app.use("/user", (req, res) => {
+  res.send("Hello From Server /user")
+})
+
+app.listen(7777, () => {
+  console.log("Server is running on port 7777")
 })
