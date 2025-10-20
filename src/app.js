7564c0ce930b0033d8ef662b1a3d46f3f767c82f
Swati Bhatia <swatibhatia626@gmail.com>
2025-10-21 02:01:13 +0530

    another way to handle error , using try catch only , best way

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ const express = require("express")
 const app = express()
 
 app.get("/getAllUser", (req, res, next) => {
-  throw new Error("Some error occurred while fetching users")
-  res.send("List of all users")
+  try {
+    throw new Error("Some error occurred while fetching users")
+    res.send("List of all users")
+  } catch (err) {
+    res.status(500).send("Something broke!")
+  }
 })
 
 app.use((err, req, res, next) => {
   console.error(err.stack)
-  if(err)
-  res.status(500).send("Something broke!")
+  if (err) res.status(500).send("Something broke using err !")
 })
 
 app.listen(7777, () => {
